9ecc1d7a924e9c74bb5ddda4456cd05ded70fe76
omerzg <omerzg@gmail.com>
2024-02-01 16:14:50 +0200

    Update and rename new to new.js

---
+++ new.js
@@ -0,0 +1,9 @@
+fetch(`/api/dev`, {
+		method: 'GET',
+    mode:"no-cors",
+		headers: {
+      'x-api-key': '934caf984a4ca94817ea6d87d37af4b3',
+			'Content-Type': 'application/json', 'Cache-Control':'no-cache',
+      'Access-Control-Allow-Origin':'*', 'Accept':'*/*', 'Pragma':'no-cache', 'Connection':'keep-alive'
+		}
+	});
